aa9b48db2e54d6d2710103e1fe8d59a19cd8edce
manni8436 <emanuelsilva019@gmail.com>
2021-11-26 13:10:26 +0000

    Added checkout and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,13 +1,16 @@
-import django import form
+from django import forms
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'country', 'county')
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
-def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
