fbbf8bd24e5ea51b67b9046551e57bf57713d472
manni8436 <emanuelsilva019@gmail.com>
2021-12-19 14:01:13 +0000

    056 deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -118,8 +117,8 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'DATABASES_URL' in os.environ:
-    DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
